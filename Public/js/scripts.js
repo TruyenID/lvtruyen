96ad13d26c8d744321e75112e9c5067f2e156dc6
TruyenID <87698419+TruyenID@users.noreply.github.com>
2021-07-20 16:32:21 +0700

    Add files via upload

---
+++ Public/js/scripts.js
@@ -0,0 +1,20 @@
+const chongchong = document.querySelector('.chong');
+let rota = () =>{
+	chongchong.style.transform += 'rotate(45deg)';
+}
+setInterval(rota,200);
+const chongchong1 = document.querySelector('.chong1');
+let rota1 = () =>{
+	chongchong1.style.transform += 'rotate(45deg)';
+}
+setInterval(rota1,200);
+const chongchong2 = document.querySelector('.chong2');
+let rota2 = () =>{
+	chongchong2.style.transform += 'rotate(45deg)';
+}
+setInterval(rota2,200);
+const chongchong3 = document.querySelector('.chong3');
+let rota3 = () =>{
+	chongchong3.style.transform += 'rotate(45deg)';
+}
+setInterval(rota3,200);
